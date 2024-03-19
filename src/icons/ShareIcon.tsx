721c5df3cd5b1c7acac8d359615e96718acb9ba4
dmswnlee <dlrmawnl03@gmail.com>
2024-03-19 17:28:23 +0900

    Layout : ProductDetailPage

---
+++ src/icons/ShareIcon.tsx
@@ -0,0 +1,5 @@
+import { IoShareOutline } from "react-icons/io5";
+
+export default function ShareIcon() {
+	return <IoShareOutline className="text-2xl" />
+}
--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -1,7 +1,25 @@
-import { useParams } from "react-router-dom";
+import ShareIcon from "../icons/ShareIcon";
+import BookmarkIcon from "../icons/BookmarkIcon";
+import ProductImage from "../components/productDetail/ProductImage";
+import ProductInfo from "../components/productDetail/ProductInfo";
+import ReservationCard from "../components/productDetail/ReservationCard";
 
 export default function ProductDetailPage() {
-  const { id } = useParams();
 
-  return <div>ProductDetailPage id: {id}</div>;
+	return (
+		<section className="w-[1120px] mx-auto relative p-4">
+			<div className="flex justify-between mb-2">
+				<p className="text-2xl font-semibold">상세페이지</p>
+				<div className="flex gap-4">
+					<ShareIcon />
+					<BookmarkIcon />
+				</div>
+			</div>
+			<ProductImage />
+			<div className="flex gap-20 mt-10">
+				<ProductInfo />
+				<ReservationCard />
+			</div>
+		</section>
+	);
 }
