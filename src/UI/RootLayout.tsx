b2c3a2fb9841ad5f3a3cfd3a894c186dda98e4b9
hyunsunglimm <gustjd3834@naver.com>
2024-03-19 21:43:29 +0900

    Feat : 로그인, 로그아웃 시 트리거될 로직 추가
    로그인 시 zustand store의 loginUser에 담고 홈페이지로 리다이렉트 시킵니다.
    로그아웃 시 loginUser를 null로 하고 로그인페이지로 리다이렉트 시킵니다.

--- src/UI/RootLayout.tsx
+++ src/UI/RootLayout.tsx
@@ -1,7 +1,31 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useNavigate } from "react-router-dom";
 import Header from "../components/layout/Header";
+import { useEffect } from "react";
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+import { getUsers } from "../firebase/firebase";
+import { useUserStore } from "../store/user";
+import { LoginUser } from "../types/user";
 
 export default function RootLayout() {
+  const navigate = useNavigate();
+  const { setLoginUser } = useUserStore();
+
+  useEffect(() => {
+    const unsubscribe = onAuthStateChanged(getAuth(), (user) => {
+      if (user) {
+        getUsers()
+          .then((users) => users.find((u: LoginUser) => u.email === user.email))
+          .then((user) => setLoginUser(user))
+          .then(() => navigate("/"));
+      } else {
+        setLoginUser(null);
+        navigate("/login");
+      }
+    });
+
+    return () => unsubscribe();
+  }, [navigate, setLoginUser]);
+
   return (
     <section>
       <Header />
