a2956a8f91695bc55a878c23e76791af8d891d3c
dmswnlee <dlrmawnl03@gmail.com>
2024-03-20 17:55:42 +0900

    Add : 카테고리 선택 기능 추가
    선택에 따른 필터링 기능은 추후 데이터 정보와 함께 추가할 예정입니다.

---
+++ src/components/homepage/FilterCategory.tsx
@@ -0,0 +1,74 @@
+import { Swiper, SwiperClass, SwiperSlide } from "swiper/react";
+import { Navigation } from "swiper/modules";
+import "swiper/css";
+import { FaArrowLeft, FaArrowRight } from "react-icons/fa";
+import { useState } from "react";
+
+type FiltersArray = {
+	filters: string[];
+};
+
+export default function FilterCategory({ filters }: FiltersArray) {
+	const [swiper, setSwiper] = useState<SwiperClass>();
+	const [activeFilter, setActiveFilter] = useState<number | null>(0);
+
+	const btnCustom = "p-2 rounded-full bg-gray-100 hover:brightness-90 absolute top-1/2 transform -translate-y-1/2";
+
+	const handlePrev = () => {
+		swiper?.slidePrev();
+	};
+	const handleNext = () => {
+		swiper?.slideNext();
+	};
+
+	const handleClickFilter = (index: number) => {
+		setActiveFilter(index);
+	};
+
+	return (
+		<div className="relative">
+			<Swiper
+				modules={[Navigation]}
+				navigation
+				slidesPerView={1}
+				onSwiper={e => {
+					setSwiper(e);
+				}}
+				breakpoints={{
+					414: {
+						slidesPerView: 2,
+					},
+					768: {
+						slidesPerView: 3,
+					},
+					1024: {
+						slidesPerView: 4,
+					},
+					1366: {
+						slidesPerView: 6,
+					},
+				}}
+				className="relative w-8/12 gap-4 mb-4">
+				{filters.map((filter, index) => (
+					<SwiperSlide key={index}>
+						<button
+							onClick={() => handleClickFilter(index)}
+							className={`w-[150px] font-semibold p-2 rounded-full ${
+								activeFilter === index ? "bg-[#F42C5B] text-white" : "bg-gray-100 text-black"
+							}`}>
+							{filter}
+						</button>
+					</SwiperSlide>
+				))}
+			</Swiper>
+			<div>
+				<button onClick={handlePrev} className={`${btnCustom} left-2`}>
+					<FaArrowLeft />
+				</button>
+				<button onClick={handleNext} className={`${btnCustom} right-2`}>
+					<FaArrowRight />
+				</button>
+			</div>
+		</div>
+	);
+}
--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -2,8 +2,10 @@ import { useQuery } from "@tanstack/react-query";
 import { getStaies } from "../util/http";
 import AccommodationCard from "../components/homepage/AccommodationCard";
 import { AccommodationInfo } from "../types/AccommodationInfo";
+import FilterCategory from "../components/homepage/FilterCategory";
 
 const filters = [
+	"전체",
 	"관광호텔",
 	"콘도미니엄",
 	"유스호스텔",
@@ -21,15 +23,13 @@ export default function HomePage() {
 		queryKey: ["accommodation"],
 		queryFn: getStaies,
 	});
+	// const [filter, setFilter] = useState(filters[0]);
+	// const filtered = getFilteredItems(accommodation, filter);
 
 	return (
 		<div className="p-4">
 			{isLoading && <p>Loading...</p>}
-			<div className="flex justify-center flex-wrap gap-4">
-				{filters.map(filter => (
-					<p className="text-lg font-semibold p-4">{filter}</p>
-				))}
-			</div>
+			<FilterCategory filters={filters} />
 			{accommodation && (
 				<ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-5 gap-y-4">
 					{accommodation.map(item => (
@@ -40,3 +40,11 @@ export default function HomePage() {
 		</div>
 	);
 }
+
+// function getFilteredItems(accommodation, filter) {
+// 	if (filter === "전체") {
+// 		return accommodation;
+// 	} else if (filter == "관광호텔") {
+// 		return accommodation.filter(item => item.code === "");
+// 	} 
+// }
