b071800a51f7aa4c38d8ead6951646f883196e17
dmswnlee <dlrmawnl03@gmail.com>
2024-03-23 23:48:33 +0900

    Feat : 각 카테고리별 필터링 기능 추가

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -3,7 +3,7 @@ import { getAccommodations } from "../util/http";
 import AccommodationCard from "../components/homepage/AccommodationCard";
 import FilterCategory from "../components/homepage/FilterCategory";
 import { useInView } from "react-intersection-observer";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { AccommodationInfo } from "../types/AccommodationInfo";
 
 const filters = [
@@ -20,7 +20,21 @@ const filters = [
 	"한옥",
 ];
 
+const categoryMap: { [key: string]: string } = {
+	관광호텔: "B02010100",
+	콘도미니엄: "B02010500",
+	유스호스텔: "B02010600",
+	펜션: "B02010700",
+	모텔: "B02010900",
+	민박: "B02011000",
+	게스트하우스: "B02011100",
+	홈스테이: "B02011200",
+	서비스드레지던스: "B02011300",
+	한옥: "B02011600",
+};
+
 export default function HomePage() {
+	const [filter, setFilter] = useState(filters[0]);
 	const { ref, inView } = useInView();
 
 	const {
@@ -42,8 +56,6 @@ export default function HomePage() {
 
 	useEffect(() => {
 		if (inView && hasNextPage) {
-			console.log("fire!");
-
 			fetchNextPage();
 		}
 	}, [inView, hasNextPage, fetchNextPage]);
@@ -56,12 +68,18 @@ export default function HomePage() {
 		return <p>Error: {error.message}</p>;
 	}
 
+	const handleFilterChange = (selectedFilter: string) => {
+		setFilter(selectedFilter);
+	};
+
+	const filteredAccommodations = getFilteredItems(accommodation.pages, filter);
+
 	return (
 		<div className="p-4">
-			<FilterCategory filters={filters} />
-			{accommodation.pages && (
+			<FilterCategory filters={filters} onFilterChange={handleFilterChange} />
+			{filteredAccommodations && (
 				<ul className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 2xl:grid-cols-5 gap-5 gap-y-4">
-					{accommodation?.pages.map(page =>
+					{filteredAccommodations.map(page =>
 						page.map((item: AccommodationInfo, index: number) => {
 							if (item.length == index + 1) {
 								return <AccommodationCard innerRef={ref} key={index} accommodation={item} />;
@@ -74,4 +92,13 @@ export default function HomePage() {
 			{isFetchingNextPage && <h3>Loading...</h3>}
 		</div>
 	);
+}
+
+function getFilteredItems(accommodation: AccommodationInfo[][], filter: string) {
+	if (filter === "전체") {
+		return accommodation;
+	} else {
+		const categoryCode = categoryMap[filter];
+		return accommodation.map(item => item.filter(it => it.category === categoryCode));
+	}
 }
