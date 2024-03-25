611840e6e751b23fc4ce4fa16e004f5a66ccd571
dmswnlee <dlrmawnl03@gmail.com>
2024-03-25 21:41:21 +0900

    Layout : productDetailPage

--- src/components/productDetail/ProductImage.tsx
+++ src/components/productDetail/ProductImage.tsx
@@ -1,12 +1,30 @@
-export default function ProductImage() {
+import { AccommodationInfo } from "../../types/AccommodationInfo";
+
+interface ProductProps {
+	data: AccommodationInfo;
+}
+
+export default function ProductImage({ data }: ProductProps) {
 	return (
 		<div className="grid grid-cols-2 gap-2 rounded overflow-hidden mb-2">
-			<img src="https://yaimg.yanolja.com/v5/2022/11/24/15/1280/637f94b14db294.95748432.jpg" alt="" />
-			<div className="grid grid-cols-2 gap-2">
-				<img src="https://yaimg.yanolja.com/v5/2022/11/24/15/1280/637f94b1eb6d41.35222594.jpg" alt="" />
-				<img src="https://yaimg.yanolja.com/v5/2022/11/24/15/1280/637f94b2ce5e09.92958598.jpg" alt="" />
-				<img src="https://yaimg.yanolja.com/v5/2022/11/24/15/1280/637f94b377d439.31514653.jpg" alt="" />
-				<img src="https://yaimg.yanolja.com/v5/2022/11/24/15/1280/637f94b52175d4.63151742.jpg" alt="" />
+			<img className="w-full h-full" src={data.image1} alt={data.title} />
+			<div className="grid grid-cols-2 gap-2 overflow-hidden">
+				<img
+					src="https://images.unsplash.com/photo-1631049307264-da0ec9d70304?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+					alt=""
+				/>
+				<img
+					src="https://images.unsplash.com/photo-1631049035182-249067d7618e?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+					alt=""
+				/>
+				<img
+					src="https://images.unsplash.com/photo-1507652313519-d4e9174996dd?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+					alt=""
+				/>
+				<img
+					src="https://images.unsplash.com/photo-1572987669554-0ba2ba9aee1f?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D"
+					alt=""
+				/>
 			</div>
 		</div>
 	);
--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -1,34 +1,24 @@
-import ShareIcon from "../icons/ShareIcon";
-import BookmarkIcon from "../icons/BookmarkIcon";
+import { useQuery } from "@tanstack/react-query";
 import ProductImage from "../components/productDetail/ProductImage";
 import ProductInfo from "../components/productDetail/ProductInfo";
 import ReservationCard from "../components/productDetail/ReservationCard";
-import { useQuery } from "@tanstack/react-query";
-import { useParams } from "react-router-dom";
 import { getAccommodation } from "../util/http";
+import { useParams } from "react-router-dom";
 
 export default function ProductDetailPage() {
-  const { id } = useParams();
+	const { id } = useParams();
+	const { data } = useQuery({
+		queryKey: ["product"],
+		queryFn: () => getAccommodation(`${id}`),
+	});
 
-  const { data } = useQuery({
-    queryKey: ["accommodation", id],
-    queryFn: () => getAccommodation(id as string),
-  });
-
-  return (
-    <section className="w-[1120px] mx-auto relative p-4">
-      <div className="flex justify-between mb-2">
-        <p className="text-2xl font-semibold">상세페이지</p>
-        <div className="flex gap-4">
-          <ShareIcon />
-          <BookmarkIcon />
-        </div>
-      </div>
-      <ProductImage />
-      <div className="flex gap-20 mt-10">
-        <ProductInfo />
-        <ReservationCard />
-      </div>
-    </section>
-  );
+	return (
+		<section className="w-[1120px] mx-auto relative p-4">
+			<ProductImage data={data} />
+			<div className="flex gap-20 mt-10">
+				<ProductInfo data={data} />
+				<ReservationCard />
+			</div>
+		</section>
+	);
 }
