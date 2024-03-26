77c9fa780ae70d8b811d55001bb7875cdd12e389
dmswnlee <dlrmawnl03@gmail.com>
2024-03-26 11:48:31 +0900

    Modify : 코드 수정

--- src/pages/ProductDetailPage.tsx
+++ src/pages/ProductDetailPage.tsx
@@ -4,27 +4,20 @@ import ProductInfo from "../components/productDetail/ProductInfo";
 import ReservationCard from "../components/productDetail/ReservationCard";
 import { getAccommodation } from "../util/http";
 import { useParams } from "react-router-dom";
-import ShareIcon from "../icons/ShareIcon";
-import BookmarkIcon from "../icons/BookmarkIcon";
 
 export default function ProductDetailPage() {
 	const { id } = useParams();
-	const { data: accommodation } = useQuery({
+	const { data: accommodation, isLoading } = useQuery({
 		queryKey: ["accommodation", id],
 		queryFn: () => getAccommodation(id as string),
 	});
 
-	console.log(accommodation);
+	if (isLoading) {
+		return <p>Loading...</p>;
+	}
 
 	return (
 		<section className="w-[1120px] mx-auto relative p-4">
-			<div className="flex justify-between mb-2">
-				<p className="text-2xl font-semibold">상세페이지</p>
-				<div className="flex gap-4">
-					<ShareIcon />
-					<BookmarkIcon />
-				</div>
-			</div>
 			<ProductImage accommodation={accommodation} />
 			<div className="flex gap-20 mt-10">
 				<ProductInfo accommodation={accommodation} />
