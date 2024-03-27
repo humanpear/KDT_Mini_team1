584a99eed6ff4a773cafbf11a8afb91e123e9956
hyunsunglimm <gustjd3834@naver.com>
2024-03-27 11:39:09 +0900

    Feat : PrevIcon 제거

--- src/pages/PaymentPage.tsx
+++ src/pages/PaymentPage.tsx
@@ -1,7 +1,6 @@
 import { useQuery } from "@tanstack/react-query";
 import PaymentInfo from "../components/payment/PaymentInfo";
 import PriceInfo from "../components/payment/PriceInfo";
-import PrevIcon from "../icons/PrevIcon";
 import { useParams } from "react-router-dom";
 import { getAccommodation } from "../util/http";
 
@@ -19,7 +18,6 @@ export default function PaymentPage() {
   return (
     <section className="w-[1120px] mx-auto mt-16 relative">
       <p className="text-3xl font-bold mb-12">예약 요청</p>
-      <PrevIcon />
       <div className="flex">
         <PaymentInfo accommodation={accommodation} />
         <PriceInfo accommodation={accommodation} />
