d7098acb04429c9f5e7b00f711d7334492da58aa
dmswnlee <dlrmawnl03@gmail.com>
2024-03-28 15:51:35 +0900

    Feat : 예약 옵션 기능 추가

--- src/hooks/useToggle.ts
+++ src/hooks/useToggle.ts
@@ -1,7 +1,8 @@
 import { useState } from "react";
 
-export function useToggle(initialValue = false): [boolean, () => void] {
+export function useToggle(initialValue = false): [boolean, () => void, (value: boolean) => void] {
 	const [value, setValue] = useState(initialValue);
 	const toggleValue = () => setValue(!value);
-	return [value, toggleValue];
+	const closeToggle = (newValue: boolean) => setValue(newValue);
+	return [value, toggleValue, closeToggle];
 }
