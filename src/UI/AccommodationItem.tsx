fd91fa66ead238a2a23bd9e7b6c0285e191216cf
hyunsunglimm <gustjd3834@naver.com>
2024-03-29 21:39:11 +0900

    Add : 장바구니와 결제내역에 사용할 컴포넌트 추가

---
+++ src/UI/AccommodationItem.tsx
@@ -0,0 +1,66 @@
+import { Link } from "react-router-dom";
+import { useMutation } from "@tanstack/react-query";
+import { queryClient, removeCartItem } from "../util/http";
+import { AccommodationWithOption } from "../types/accommodationInfo";
+import { stayDuration } from "../util/date";
+
+type Props = {
+  item: AccommodationWithOption;
+  type: string;
+};
+
+export default function AccommodationItem({ item }: Props) {
+  const { title, address, image1 } = item.accommodation;
+  const { start_date, end_date, total_price } = item.reservation;
+
+  // const { mutate } = useMutation({
+  //   mutationFn: removeCartItem,
+  //   onSuccess: () => {
+  //     queryClient.invalidateQueries({
+  //       queryKey: ["carts"],
+  //     });
+  //   },
+  // });
+
+  // function handleClick() {
+  //   mutate(contentid);
+  // }
+
+  return (
+    <li className="shadow-basic p-6 rounded-md relative">
+      <div className="border-b pb-4">
+        <p>{title}</p>
+        <p className="text-[13px] text-stone-400">{address}</p>
+      </div>
+      <div className="flex pt-4">
+        <img
+          src={image1}
+          alt={`${title} 숙소 이미지`}
+          className="w-[100px] h-[100px] object-cover rounded-lg"
+        />
+        <div className="pl-4">
+          <p>
+            {start_date} ~ {end_date} |{" "}
+            {stayDuration(new Date(start_date), new Date(end_date))}박
+          </p>
+          {/* <p>인원 : {guest}명</p> */}
+        </div>
+      </div>
+      <div className="flex justify-end items-center gap-4">
+        <p className="text-end font-bold text-lg">
+          {total_price.toLocaleString()}원
+        </p>
+        {/* <Link
+          to={`/payment/${contentid}?check_in=${start_date}&check_out=${end_date}&guest=${guest}&room=${room}`}
+          className="bg-brand text-white py-1 px-2 rounded-lg"
+        >
+          예약
+        </Link> */}
+      </div>
+      {/* <CloseIcon
+        className="absolute top-6 right-6 text-2xl cursor-pointer"
+        onClick={handleClick}
+      /> */}
+    </li>
+  );
+}
